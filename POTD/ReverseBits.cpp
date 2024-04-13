5d19a31a736ccd031976fc69217bc72ecb0debd5
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-04-13 21:46:25 +0530

    April 13 POTD

---
+++ POTD/ReverseBits.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        // code here
+        long reversed = 0;
+        for(int i = 0; i < 32; i++){
+            reversed = (reversed<<1) | (x&1);
+            x>>=1;
+        }
+        return reversed;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
