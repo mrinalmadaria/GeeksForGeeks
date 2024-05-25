e83c5534d828e0aabbfb9d234e1eca44869c68dd
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-05-25 20:03:08 +0530

    May 25 POTD

---
+++ POTD/YouAndYourBooks.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+        long maxBooks = 0;
+        long currentBooks = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] <= k){
+                currentBooks += arr[i];
+                maxBooks = max(maxBooks, currentBooks);
+            } else {
+                currentBooks = 0;
+            }
+        }
+        return maxBooks;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
