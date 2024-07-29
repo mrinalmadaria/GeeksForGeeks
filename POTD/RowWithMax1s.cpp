087020197c3754f08611e18c0b2eb0220750260e
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-07-29 23:26:53 +0530

    July 29 POTD

---
+++ POTD/RowWithMax1s.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        int n = arr.size();
+        if (n == 0) return -1;
+        
+        int m = arr[0].size();
+        if (m == 0) return -1;
+        
+        int max_row_index = -1;
+        int max_1s_count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int count_1s = 0;
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 1) {
+                    count_1s++;
+                }
+            }
+            if (count_1s > max_1s_count) {
+                max_1s_count = count_1s;
+                max_row_index = i;
+            }
+        }
+        return max_row_index;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
