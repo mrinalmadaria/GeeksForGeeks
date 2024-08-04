8582984903a30e8cb1e1edf6b32f746dcfd9f2c3
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-08-04 23:18:05 +0530

    August 4 POTD

---
+++ POTD/NMeetingsInOneRoom.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    int maxMeetings(int n, int start[], int end[]) {
+        // Your code here
+        vector<pair<int, int>> vec;
+        for(int i = 0; i < n; i++){
+            vec.push_back({end[i], start[i]});
+        }
+        
+        sort(vec.begin(), vec.end());
+        int c = 1, d = vec[0].first;
+        
+        for(int i = 1; i < n; i++){
+            if(vec[i].second > d){
+                d = vec[i].first;
+                c++;
+            }
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++)
+            cin >> start[i];
+
+        for (int i = 0; i < n; i++)
+            cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(n, start, end);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
