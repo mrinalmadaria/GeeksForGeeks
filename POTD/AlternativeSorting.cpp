68d014509f0dc85fd3731bbdf2267d537f9d9c74
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-10-25 23:25:54 +0530

    October 25 POTD

---
+++ POTD/AlternativeSorting.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        // Your code goes here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        vector<int> ans;
+        
+        int i = 0, j = n-1;
+        
+        while(i < j){
+            ans.push_back(arr[j--]);
+            ans.push_back(arr[i++]);
+        }
+        if(n % 2 == 1) ans.push_back(arr[i]);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
