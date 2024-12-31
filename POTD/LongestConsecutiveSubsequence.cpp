c68d65d3101c4bc7562c88d174ebd7630b0352a1
Mrinal Madaria <mrinalmadaria3@gmail.com>
2024-12-31 11:47:35 +0530

    December 31 POTD

---
+++ POTD/LongestConsecutiveSubsequence.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& arr) {
+        // Your code here
+        sort(arr.begin(), arr.end());
+        int n = arr.size(), temp = 1, ans = 1;
+        
+        for(int i = 1; i < n; i++){
+            if(arr[i] == arr[i-1]) continue;
+            if(arr[i] == arr[i-1]+1) temp++;
+            else{
+                ans = max(ans, temp);
+                temp = 1;
+            }
+        }
+        return max(ans, temp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.longestConsecutive(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
