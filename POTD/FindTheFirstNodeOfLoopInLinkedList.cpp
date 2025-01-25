7340ba165ca613369536e1f0b13cfb8715690efd
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-01-25 23:47:14 +0530

    January 23 POTD

---
+++ POTD/FindTheFirstNodeOfLoopInLinkedList.cpp
@@ -0,0 +1,18 @@
+Node* slow = head;
+        Node* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(slow!=fast){
+            return NULL;
+        }
+        slow = head;
+        while(slow!=fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
