cd86d75adf884ab04409ff28570f688e8e8c0014
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-02-09 22:40:10 +0530

    February 9 POTD

---
+++ POTD/CountNumberOfBadPairs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        long long countBadPairs(vector<int>& nums) {
+            int n = nums.size();
+            long long totalPairs = (long long)n * (n - 1) / 2;
+            unordered_map<int, long long> diffCount;
+            for (int i = 0; i < n; i++) {
+                int diff = nums[i] - i;
+                totalPairs -= diffCount[diff];
+                diffCount[diff]++;
+            }
+            return totalPairs;
+        }
+    };
