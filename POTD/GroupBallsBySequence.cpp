1df7f3ba83638ce76b11cadead79eb5486c1073f
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-06-20 23:50:13 +0530

    June 20 POTD

---
+++ POTD/GroupBallsBySequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    bool validgroup(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        if(n%k!=0){
+            return false ;
+          }
+          sort(arr.begin(),arr.end());
+          unordered_map<int, int>mp;
+          for(int i=0 ; i<n ; i++){
+            mp[arr[i]]++;  
+          }
+          for(int i=0 ; i<n ; i++){
+              if(mp[arr[i]]==0){
+                  continue;
+              }
+              for(int j=0 ; j<k ; j++){
+                 if(mp[arr[i]+j]==0) 
+                 return false ;
+                 mp[arr[i]+j]--;
+              }
+          }
+          return true ;
+    }
+};
