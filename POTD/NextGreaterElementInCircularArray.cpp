5c778523ad285fad6035692762bc4b055f95fee0
Mrinal Madaria <mrinalmadaria3@gmail.com>
2025-07-07 22:38:27 +0530

    July 7 POTD

---
+++ POTD/NextGreaterElementInCircularArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int> res(n, -1);
+        stack<int> stk;
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!stk.empty() && stk.top() <= arr[i % n]) {
+                stk.pop();
+            }
+            if (i < n && !stk.empty()) {
+                res[i] = stk.top();
+            }
+            stk.push(arr[i % n]);
+        }
+        return res;
+    }
+};
